24f5213e0a91deadbc4f168ba19b70df795ab667
unknown <timothy.xue@hotmail.com>
2018-01-11 00:05:41 +0800

    learnjvm:add two instructions as nop one and branch instruction

--- ch05/instructions/base/instruction.go
+++ ch05/instructions/base/instruction.go
@@ -2,7 +2,24 @@ package base
 
 import "learnjvmgo/ch05/rtda"
 
+//Instruction #
 type Instruction interface {
 	FetchOprands(reader *ByteCodeReader)
 	Execute(frame *rtda.Frame)
+}
+
+//NoOperandsInstruction #
+type NoOperandsInstruction struct{}
+
+func (noi *NoOperandsInstruction) FetchOperands(reader *BytecodeReader) {
+	//nothing to do
+}
+
+//BranchInstruction #
+type BranchInstruction struct {
+	Offset int
+}
+
+func (bi *BranchInstruction) FetchOperands(reader *BytecodeReader) {
+	bi.Offset = int(reader.ReadInt16())
 }
